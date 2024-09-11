7b24db4c926002c5ada0aee14f31d920bcbb7c60
zhenzhen <wzzpdl@gmail.com>
2024-09-11 13:06:19 -0600

    Week2 assignments
    Week 2 assignments  to change the week 2 folder and root page

---
+++ app/week-2/student-info.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+ 
+export default function StudentInfo () {
+  return (
+    <div>
+      <p>Zhenzhen Wu</p>
+      <Link href="https://github.com/Jennawu2023?tab=repositories">https://github.com/Jennawu2023?tab=repositories</Link>
+    </div>
+  );
+}
+
