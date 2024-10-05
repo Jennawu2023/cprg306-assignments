8ca26c42412eb90dcda0f7057c232c32b2409da9
zhenzhen <wzzpdl@gmail.com>
2024-10-05 16:35:33 -0600

    week 5 assignments

---
+++ app/week-5/page.js
@@ -0,0 +1,10 @@
+import NewItem from './new-item';
+
+export default function Page() {
+    return(
+        <main className="bg-black p-4 flex justify-center">
+            <NewItem/>
+        </main>
+    );
+
+}
